4eb27194b318947827ea181ec46723e5156a2ff2
Yonghoon <yonghoonj98@gmail.com>
2022-05-28 22:48:29 +0900

    Add(env): 서버 초기 설정 및 환경 변수 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 외부 클라우드에서 불러오는 비동기 작업
+// const getEnv = async () => {
+//   const response = await axios.get('/비밀키요청');
+
+//   return response.data;
+// };
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [] })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
