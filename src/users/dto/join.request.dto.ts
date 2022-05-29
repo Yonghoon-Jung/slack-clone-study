6d20679f884a5db6cdb40bbdc732d60a71825a41
Yonghoon <yonghoonj98@gmail.com>
2022-05-29 15:45:23 +0900

    Add(decorater): 기본 데코레이터 Query, Body, Param 적용

---
+++ src/users/dto/join.request.dto.ts
@@ -0,0 +1,17 @@
+/*
+class와 인터페이스의 차이점
+class는 대표적으로 상속이 가능하고 기존 class와 유사하게 사용이 가능
+interface는 사실상 TS에만 존재하고 TS 컴파일 종료 후 사라짐
+class는 JS 변환 후에도 남아있음
+
+interface는 런타임에서 날라가서 체크같은 게 어려운데
+class는 class 자체가 타입 역할도 해줘서 인터페이스와 비슷한 역할을 해주면서도
+JS로 변환 후에도 계속 남아있어서 JS 단에서도 타입 검증, 발리데이션 같은 걸 수행가능
+*/
+export class JoinRequestDto {
+  public email: string;
+
+  public nickname: string;
+
+  public password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('api/users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
 
   @Post()
-  postUsers() {}
+  postUsers(@Body() data: JoinRequestDto) {
+    const res = this.usersService.postUsers(
+      data.email,
+      data.nickname,
+      data.password,
+    );
+    return res;
+  }
 
   @Post('login')
   logIn() {
