80fdd8cffada70e80ff3dd037bea9bf33549b0b6
Yonghoon <yonghoonj98@gmail.com>
2022-05-29 15:57:39 +0900

    Add(api): 컨트롤러에 초기 api 작성

---
+++ src/workspaces/workspaces.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Delete, Get, Post } from '@nestjs/common';
+
+@Controller('api/workspaces')
+export class WorkspacesController {
+  @Get()
+  getMyWorkspaces() {}
+
+  @Post()
+  createWorkspace() {}
+
+  @Get(':url/members')
+  getAllMembersFromWorkspace() {}
+
+  @Post(':url/members')
+  inviteMembersToWorkspace() {}
+
+  @Delete(':url/members/:id')
+  kickMemberFromWorkspace() {}
+
+  @Get(':url/members/:id')
+  getMemberInfoInWorkspace() {}
+
+  @Get(':url/users/:id')
+  // 나중에 제거해야 한다고 알려주는 문 DEPRECATED_
+  DEPRECATED_getMemberInfoInWorkspace() {
+    this.getMemberInfoInWorkspace();
+  }
+}
